36de4b2e0315865c1d1a29ac6557d506cc4878c3
PrathamBansal012 <nehabansalbansal22@gmail.com>
2026-06-09 20:32:20 +0530

    Add files via upload

---
+++ lite 2.py
@@ -0,0 +1,15 @@
+
+import sqlite3
+import pandas as pd
+
+database = 'database.sqlite'  
+
+conn = sqlite3.connect(database)
+
+print("Opened database successfully")
+
+tables = pd.read_sql("""
+SELECT *
+FROM sqlite_master
+WHERE type='table';
+""", conn)
