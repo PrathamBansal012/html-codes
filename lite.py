42d14b1fc685d1e6dbe04d5855bcf97fd72ca50a
PrathamBansal012 <nehabansalbansal22@gmail.com>
2026-06-09 20:20:38 +0530

    Add files via upload

---
+++ lite.py
@@ -0,0 +1,7 @@
+print("what is the type of the above given database? - \n 1)Relational database \n 2)non-Relational database")
+awnser=int(input("enter your guesse here"))
+if awnser == 2:
+    print("you guessed it right")
+else:
+    print("unfortunately it was wrong")
+print("tell your mentor why you  guessed this")
