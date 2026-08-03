8e1b73a78cbfce1b7445f0c4cc34542f75316c2a
PrathamBansal012 <nehabansalbansal22@gmail.com>
2026-08-03 20:34:54 +0530

    Add files via upload

---
+++ pie xhart2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_csv('Weather Dataset.csv')
+df.head()
+df_group = df.groupby('month').mean(numeric_only=True)
+df_group = df_group.reset_index()
+df_group.plot.area(x='month', y='Humidity', alpha=0.6)
+plt.plot(df['Temperature (C)'])
+plt.ylabel('Temperature (C)')
+plt.xlabel('Reading Number over Time')
+plt.show()
