76c9ab3bd6ca38bc6079e248e6529798bf1d052e
PrathamBansal012 <nehabansalbansal22@gmail.com>
2026-08-25 20:25:40 +0530

    Add files via upload

---
+++ rar3.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+data=pd.read_csv("Titanic Dataset.csv")
+"import database"
+print(data.head(5))
+print(data.isnull().sum())
+data.dtypes
+nominal_cat = ['Name', 'Ticket', 'Cabin']
+
+ordinal_cat = ['Embarked', 'Gender']
+
+data['Gender'].value_counts()
+
+gender_categories = ['Female', 'Male']
+
+data['Gender'] = pd.Categorical(data['Gender'], gender_categories, ordered=True)
+
+median_index = np.median(data['Gender'].cat.codes)
+median_gender = gender_categories[int(median_index)]
+print(median_gender)
+
+data['Embarked'].value_counts()
+
+embarked_categories = ['S', 'C', 'Q']
+
+data['Embarked'] = pd.Categorical(data['Embarked'], embarked_categories, ordered=True)
+median_index = np.median(data['Embarked'].cat.codes)
+median_embarked = embarked_categories[int(median_index)]
+print(median_embarked)
