ab73d8f822c2b6549d82f49fba0610d8837b790b
PrathamBansal012 <nehabansalbansal22@gmail.com>
2026-09-08 20:26:32 +0530

    Add files via upload

---
+++ fah9.py
@@ -0,0 +1,6 @@
+set1={'A','B','C','D','E'}
+set2={'D','B','Y','x','v','Z'}
+union=set1.intersection(set2)
+total_guests=list(union)
+print("total guest ar:",len(total_guests))
+print("guests",total_guests)
